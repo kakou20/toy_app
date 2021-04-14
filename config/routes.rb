28862472a4206a7918e793154ab58c6eae59d65a
Kou Ka <a2013hh@aiit.ac.jp>
2021-04-14 14:15:58 +0000

    Finish ch02 toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  resources :microposts
+  resources :users
+  #added by Ka
+  #get 'user#index'
+  #get 'microposts#index'
+  #get 'microposts#edit'
+  #root 'application#hello'
 end
